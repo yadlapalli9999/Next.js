4fbf1386e5f38f395f874b47b1032a6ad413e961
yadlapalli9999 <yadlapalli9999@gmail.com>
2022-09-04 21:04:43 +0530

    list products

--- next-ecommerce/pages/index.js
+++ next-ecommerce/pages/index.js
@@ -1,7 +1,9 @@
+import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Grid, Typography } from '@mui/material'
 import Head from 'next/head'
 import Image from 'next/image'
 import Layout from '../components/Layout'
 import styles from '../styles/Home.module.css'
+import data from '../utils/data'
 
 export default function Home() {
   return (
@@ -14,11 +16,27 @@ export default function Home() {
       </Head>
       <div>
         <h1>Products</h1>
-        <ul>
-          <li>product1</li>
-          <li>product2</li>
-          <li>product3</li>
-        </ul>
+        <Grid container spacing={3}>
+          {
+            data.products.map((product)=>(
+              <Grid item md={4} key={product.name}>
+                <Card>
+                  <CardActionArea>
+                    <CardMedia component="img" image={product.image} title={product.name}/>
+                  <CardContent>
+                    <Typography>{product.name}</Typography>
+                  </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Typography>${product.price}</Typography>
+                    <Button size="small" color='primary'>Add To Cart</Button>
+                  </CardActions>
+
+                </Card>
+              </Grid>
+            ))
+          }
+        </Grid>
       </div>
     </div>
     </Layout>
