13a61cf8f024223637cfb95de07432e84d92e553
yadlapalli9999 <yadlapalli9999@gmail.com>
2022-09-04 21:28:07 +0530

    add header link

--- next-ecommerce/components/Layout.js
+++ next-ecommerce/components/Layout.js
@@ -1,19 +1,38 @@
 import React from "react";
 import Head from "next/head";
-import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+import { AppBar, Container, Link, Toolbar, Typography } from "@mui/material";
+import NextLink from 'next/link';
 import UseStyles from "../utils/styles";
 
 
-export default function Layout({children}){
+export default function Layout({children,title,description}){
     const classes = UseStyles();
     return(
         <div>
             <Head>
-                <title>Next Ecommerce</title>
+              <title>{title ? `${title} - Next Amazona` : 'Next Amazona'}</title>
+              {description && <meta name="description" content={description}></meta>}            
             </Head>
             <AppBar position="static" className={classes.navbar}>
                 <Toolbar>
-                    <Typography>Next eCommerce</Typography>
+                    <NextLink href="/" passHref>
+                        <Link>
+                        <Typography className={classes.brand}>Next eCommerce</Typography>
+                        </Link>
+                    </NextLink>
+                    <div className={classes.grow}></div>
+                    <div>
+                        <NextLink href="/cart" passHref>
+                            <Link>
+                               Cart
+                            </Link>
+                        </NextLink>
+                        <NextLink href="/login" passHref>
+                            <Link>
+                               Login
+                            </Link>
+                        </NextLink>
+                    </div>
                 </Toolbar>
             </AppBar>
             <Container className={classes.main}>
--- next-ecommerce/utils/styles.js
+++ next-ecommerce/utils/styles.js
@@ -8,6 +8,13 @@ const UseStyles = makeStyles({
             marginLeft:10
         }
     },
+    brand:{
+        fontWeight:'bold',
+        fontSize:'1.5rem'
+    },
+    grow:{
+       flexGrow:1
+    },
     main:{
         minHeight:'80vh'
     },
