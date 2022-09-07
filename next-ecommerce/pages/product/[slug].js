9e57a9289c0d6a3f7be9e3861cb7a987ba68bab9
yadlapalli9999 <yadlapalli9999@gmail.com>
2022-09-07 08:40:57 +0530

    9 create product details page

--- next-ecommerce/pages/product/[slug].js
+++ next-ecommerce/pages/product/[slug].js
@@ -1,11 +1,11 @@
-import { ClassNames } from "@emotion/react";
 import { useRouter } from "next/router";
 import React from "react";
 import NextLink from 'next/link';
+import Image from 'next/image'
 import Layout from "../../components/Layout";
 import data from "../../utils/data";
 import UseStyles from "../../utils/styles";
-import { Link } from "@mui/material";
+import { Link,Grid, List, ListItem, Typography, Card, Button } from "@mui/material";
 
 
 
@@ -21,11 +21,52 @@ export default function ProductDetail(){
         <Layout title={product.name}>
            <div className={classes.section}>
              <NextLink href="/" passHref>
-                <Link>Back to Products</Link>
+                <Link>
+                   <Typography>Back to Products</Typography></Link>
              </NextLink>
            </div>
-           <Grid container spacing={3}>
-             <Grid item md={3}></Grid>
+           <Grid container spacing={1}>
+             <Grid item md={6} xs={12}>
+                <Image src={product.image} alt={product.name} Layout="responsive" width={640} height={640}/>
+             </Grid>
+             <Grid item md={3} xs={12}>
+                <List>
+                    <ListItem>
+                        <Typography>Category: {product.category}</Typography>
+                    </ListItem>
+                    <ListItem>
+                        <Typography>Brand: {product.brand}</Typography>
+                    </ListItem>
+                    <ListItem>
+                        <Typography>Rating :{product.rating} stars ({product.numReviews} reviews)</Typography>
+                    </ListItem>
+                    <ListItem>
+                        <Typography>Description: {product.description}</Typography>
+                    </ListItem>
+                </List>
+             </Grid>
+             <Grid item md={3} xs={12}>
+                <Card>
+                    <List>
+                        <ListItem>
+                            <Grid container>
+                                <Grid item xs={6}><Typography>Price</Typography></Grid>
+                                <Grid item xs={6}>${product.price}</Grid>
+                            </Grid>
+                           
+                        </ListItem>
+                        <ListItem>
+                        <Grid container>
+                                <Grid item xs={6}><Typography>Status</Typography></Grid>
+                                <Grid item xs={6}>{product.countInStock >0 ? 'In Stock':'Unavaliable'}</Grid>
+                            </Grid>
+                        </ListItem>
+                        <ListItem>
+                            <Button fullWidth variant="contained" color="primary">Add to Cart</Button>
+                        </ListItem>
+                    </List>
+                </Card>
+             </Grid>
            </Grid>
         </Layout>
     )
