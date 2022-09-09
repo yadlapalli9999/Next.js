b5187b5b5b4303409f6b7d415471775d032b34ee
yadlapalli9999 <yadlapalli9999@gmail.com>
2022-09-09 16:53:14 +0530

    connect mongoose

--- next-ecommerce/pages/api/hello.js
+++ next-ecommerce/pages/api/hello.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import db from "../../utils/db"
+
 export default function handler(req, res) {
+  db.connect()
   res.status(200).json({ name: 'John Doe' })
 }
